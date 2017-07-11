986425269e18fd4411411501a48ff70a554536d5
archermind <test@test.com>
2017-07-11 11:36:49 +0800

    ttt

---
+++ src/main/java/com/archermind/main/Demo.java
@@ -0,0 +1,13 @@
+package com.archermind.main;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by archermind on 7/10/17.
+ */
+public class Demo {
+    @Test
+    public static void test_1(){
+        System.out.println("test 1");
+    }
+}
